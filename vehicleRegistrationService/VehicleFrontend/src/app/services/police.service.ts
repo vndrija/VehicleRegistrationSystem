2ab401c5924543f98465615dee7f9b4fb5ac55c6
vndrija <matijasevicicon@gmail.com>
2026-02-19 17:06:25 +0100

    Add services on the frontend

---
+++ vehicleRegistrationService/VehicleFrontend/src/app/services/police.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+export interface TrafficViolation {
+  id: number;
+  registrationNumber: string;
+  ownerName: string;
+  violationType: string;
+  location: string;
+  dateOfViolation: string;
+  fineAmount: number;
+  status: string;
+}
+
+export interface PoliceVehicleReport {
+  registrationNumber: string;
+  isWanted: boolean;
+  wantedReason: string | null;
+  activeViolations: TrafficViolation[];
+  violationCount: number;
+  outstandingFines: number;
+  status: 'Clear' | 'Alert' | 'Wanted';
+  checkedAt: string;
+}
+
+export interface PoliceCheckResponse {
+  message: string;
+  data: PoliceVehicleReport;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PoliceService {
+  private http = inject(HttpClient);
+  private apiUrl = environment.apiConfig.vehicleService;
+
+  /**
+   * Check vehicle status with police
+   * Returns if vehicle is wanted, has violations, or is clear
+   */
+  checkVehicleWithPolice(vehicleId: number): Observable<any> {
+    return this.http.get<PoliceCheckResponse>(`${this.apiUrl}/${vehicleId}/police-check`);
+  }
+
+  /**
+   * Report vehicle to police
+   * Sends vehicle information to police database
+   */
+  reportVehicleToPolice(vehicleId: number): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/${vehicleId}/report-to-police`, {});
+  }
+}
